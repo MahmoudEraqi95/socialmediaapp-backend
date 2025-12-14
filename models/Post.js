587471c3e13d064300f38648307f593c055e02be
Eraqi <mahmouderaqi225@yahoo.com>
2025-12-13 18:26:44 -0600

    updated image url to not use a real url

--- models/Post.js
+++ models/Post.js
@@ -1,12 +1,15 @@
 
 class Post {
   constructor(id, user, content, imageUrl = null, likes = 0) {
+    var date = new Date()
     this.id = id;
     this.user = user;
     this.content = content;
     this.imageUrl = imageUrl;
     this.likes = likes;
-    this.createdAt = new Date();
+    this.createdAt = date;
+    this.updatedAt = date;
+    this.deleted = false;
   }
 }
 
@@ -18,7 +21,7 @@ for (let i = 1; i <= 100; i++) {
       i,
       `User_${i}`,
       `This is post number ${i}`,
-      i % 5 === 0 ? `https://example.com/image${i}.jpg` : null,
+      i % 5 === 0 ? `fake image url` : null,
       Math.floor(Math.random() * 100)
     )
   );
